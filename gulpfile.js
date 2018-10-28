f2258c014cc89aa80650db534323bd926d718bbc
Beto <jrs_robert@hotmail.com>
2018-10-28 12:45:44 -0300

    Add minify flag to output css task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ const gulp = require('gulp'),
 
 gulp.task('sass', () => 
     gulp.src('src/sass/**/*.scss')
-    .pipe( sass().on('error', sass.logError) )
+    .pipe( sass({outputStyle: 'compressed'}).on('error', sass.logError) )
     .pipe(gulp.dest('dist/css'))
     .pipe( browserSync.stream() )
 );
